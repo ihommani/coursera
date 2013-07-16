75b638a27599cdc498087abb0e2fd00b9e6bfc19
ihommani <ismaelhommani@gmail.com>
2013-07-16 23:53:37 +0200

    Python implementation of merge-sort

--- Algorithms: Design and Analysis/Week1/divide_and_conquer_algorithm.py
+++ Algorithms: Design and Analysis/Week1/divide_and_conquer_algorithm.py
@@ -2,21 +2,43 @@
 #-*- coding: ISO-8859-15 -*-
 
 class Result:
-    def __init__(self, inversions, merged_table):
+    def __init__(self, inversions, merged_array):
         self.inversions=inversions
 
-class Merge:
-    def __init__(self, table1, table2):
-        self.table1 = table1
-        self.table2 = table2
-        self.table3
+class MergeArray:
 
-    def merge(self):
-        for i in table1:    
-        return table
+    def sort(self, array):
+        if len(array) > 2:
+            return self.merge(self.sort(array[:len(array)/2]), self.sort(array[len(array)/2:]))
+        return self.swap(array)
+
+    def merge(self, array1, array2):
+        array3 = []
+        for k in range(len(array1) + len(array2)):    
+            if len(array1) == 0 and len(array2) > 0:
+                array3.append(array2.pop(0))
+            elif len(array2) == 0 and len(array1) > 0:
+                array3.append(array1.pop(0))
+            elif array1[0] < array2[0]:
+                array3.append(array1.pop(0))
+            else:
+                array3.append(array2.pop(0))
+        return array3
+
+    def swap(self, array):
+        if len(array) == 1:
+            return array
+        elif array[0] > array[1]:
+            temp = array[1]
+            array[1] = array[0]
+            array[0] = temp
+        return array     
 
 
-
-if name == "__main__":
+if __name__ == "__main__":
+    t = [341, 2, 6, 434, 23, 45, 5, 7, 12]
+    t = MergeArray().sort(t)
+    print t
+    
     
 
