5cde5e2c08649f8d978502eecf30dcb7f1bff744
ihommani <ismaelhommani@gmail.com>
2013-08-02 23:14:39 +0200

    Unit test for quickSort

---
+++ Algorithms: Design and Analysis/week2/test.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#-*- coding: ISO-8859-15 -*-
+
+import unittest
+import random
+from quick_sort import QuickSortArray
+
+
+class TestMergeSort(unittest.TestCase):
+    
+    def setUp(self):
+        self.toSort = range(100)
+        random.shuffle(self.toSort)
+
+    def test_shuffle_first(self):
+        sorter = QuickSortArray(1)
+        self.assertEqual(sorter.sort(self.toSort), range(100))
+
+    def test_shuffle_last(self):
+        sorter = QuickSortArray(2)
+        self.assertEqual(sorter.sort(self.toSort), range(100))
+
+    def test_shuffle_middle(self):
+        sorter = QuickSortArray(3)
+        self.assertEqual(sorter.sort(self.toSort), range(100))
+
+    @unittest.expectedFailure
+    def test_shuffle_middle(self):
+        sorter = QuickSortArray(3)
+        self.assertEqual(sorter.sort(self.toSort), range(101))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
